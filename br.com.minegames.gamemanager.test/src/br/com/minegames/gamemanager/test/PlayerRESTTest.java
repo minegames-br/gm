71d0cd1417317505c23e59538dc07abedb4db7b7
Joao Emilio <joaoemilio@gmail.com>
2016-10-22 12:11:16 -0200

    preparando o game manager

--- br.com.minegames.gamemanager.test/src/br/com/minegames/gamemanager/test/PlayerRESTTest.java
+++ br.com.minegames.gamemanager.test/src/br/com/minegames/gamemanager/test/PlayerRESTTest.java
@@ -7,15 +7,22 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
+import org.glassfish.jersey.apache.connector.ApacheConnectorProvider;
+import org.glassfish.jersey.client.ClientConfig;
+
 import br.com.minegames.core.domain.GamePlayer;
 import br.com.minegames.core.json.JSONParser;
 
 public class PlayerRESTTest {
 
 	public static void main(String args[]) {
+		
+		ClientConfig clientConfig = new ClientConfig();
+        clientConfig.connectorProvider(new ApacheConnectorProvider());
+
 		String restURL = "http://localhost:8080/gamemanager/webresources";
 		//String restURL = "http://jamine-bot.mybluemix.net/webresources";
-        Client client = ClientBuilder.newClient();
+        Client client = ClientBuilder.newClient(clientConfig);
         WebTarget target = client.target(restURL);
 		
 		GamePlayer domain = new GamePlayer();
