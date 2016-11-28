143ad97c98bdd582477934302f2cbcfc932f76ea
Joao Emilio <joaoemilio@gmail.com>
2016-11-28 11:11:37 -0200

    admin client initial load

---
+++ com.thecraftcloud.admin.client/src/test/java/com/thecraftcloud/admin/test/GetGameTest.java
@@ -0,0 +1,29 @@
+package com.thecraftcloud.admin.test;
+
+import org.junit.Test;
+
+import com.thecraftcloud.admin.socket.client.AdminClient;
+import com.thecraftcloud.core.admin.domain.ActionDTO;
+import com.thecraftcloud.core.admin.domain.ResponseDTO;
+import com.thecraftcloud.core.domain.ServerInstance;
+
+public class GetGameTest {
+
+	@Test
+	public void test() {
+		ActionDTO dto = new ActionDTO();
+		dto.setName(ActionDTO.GET_GAME);
+	
+		try{
+			ServerInstance server = new ServerInstance();
+			server.setIp_address("localhost");
+			server.setAdminPort(65000);
+			AdminClient client = new AdminClient();
+			ResponseDTO responseDTO = client.execute(server, dto); 
+			System.out.println( server.getName() + " - " + responseDTO.getMessage() + " " + responseDTO.getResult() );
+		}catch(Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+}
